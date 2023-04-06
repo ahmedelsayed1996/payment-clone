da83be6ffd67cd7a88c30fb99de1bee34acd0204
Ahmed El-Ged <121124311+ahmedelsayed1996@users.noreply.github.com>
2023-04-06 14:20:10 +0200

    payment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
 
 function App() {
+  // window.paypal.Buttons().render('#paypall-btn')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div id="paypall-btn">
+      </div> */}
+      <PayPalScriptProvider options={{ "client-id": "AV2sG0LhfJ-7W5B83X-Y5COB439khvH62wyQoODXTit-zZNkWSZlRYI-lzmQZvWzc_Gs6KPpRMZRir7B" }}>
+        <PayPalButtons createOrder={(data, action) => {
+          return action.order.create({
+            purchase_units: [{
+              amount: {
+                value: "600.00"
+              },
+            },
+            ],
+          },
+          );
+        }}
+          onApprove={(data, action) => {
+            return action.order.capture().then((details) => {
+              alert("transaction is completed bt");
+            })
+          }} />
+      </PayPalScriptProvider>
+      {/* <PayPalScriptProvider options={{ "client-id": "AV2sG0LhfJ-7W5B83X-Y5COB439khvH62wyQoODXTit-zZNkWSZlRYI-lzmQZvWzc_Gs6KPpRMZRir7B" }} >
+        <PayPalButtons />
+      </PayPalScriptProvider> */}
+    </>
   );
 }
+
 
 export default App;
